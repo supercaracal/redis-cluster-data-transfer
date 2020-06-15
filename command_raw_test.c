4398a48726723f82bbea58b515f2f4fb55022ee5
Taishi Kasuga <kasuga@livesense.co.jp>
2020-06-15 16:27:36 +0900

    Fix test case

--- command_raw_test.c
+++ command_raw_test.c
@@ -48,11 +48,11 @@ static void TestCommandRawParseRawReply002(void) {
   int i, ret;
 
   struct {
-    char *buf1; int size1; int expRC1; char *buf2; int size2; int expRC2; char *expL; int expLS;
+    char *buf1; int size1; int expRC1; char *buf2; int size2; int expRC2; int expN; char *expL; int expLS; ReplyType expLT;
   } c[3] = {
-    {"+O", 2, NEED_MORE_REPLY, "K\r\n", 3, MY_OK_CODE, "OK", 2},
-    {"-ERR ", 5, NEED_MORE_REPLY, "you wrong\r\n", 11, MY_OK_CODE, "ERR you wrong", 13},
-    {":123", 4, NEED_MORE_REPLY, "45\r\n", 4, MY_OK_CODE, "12345", 5},
+    {"+O", 2, NEED_MORE_REPLY, "K\r\n", 3, MY_OK_CODE, 1, "OK", 2, STRING},
+    {"-ERR ", 5, NEED_MORE_REPLY, "you wrong\r\n", 11, MY_OK_CODE, 1, "ERR you wrong", 13, ERR},
+    {":123", 4, NEED_MORE_REPLY, "45\r\n", 4, MY_OK_CODE, 1, "12345", 5, INTEGER},
   };
 
   Reply r, *reply;
@@ -69,8 +69,18 @@ static void TestCommandRawParseRawReply002(void) {
     printf("%s", ret == c[i].expRC2 ? TEST_OK : TEST_NG);
     printf(" TestCommandRawParseRawReply002: return code: expected: %d, actual: %d\n", c[i].expRC2, ret);
 
+    printf("%s", reply->i == c[i].expN ? TEST_OK : TEST_NG);
+    printf(" TestCommandRawParseRawReply002: number of reply lines: expected: %d, actual: %d\n", c[i].expN, reply->i);
+
+    printf("%s", reply->sizes[0] == c[i].expLS ? TEST_OK : TEST_NG);
+    printf(" TestCommandRawParseRawReply002: reply line size: expected: %d, actual: %d\n", c[i].expLS, reply->sizes[0]);
+
     printf("%s", strncmp(reply->lines[0], c[i].expL, c[i].expLS) == 0 ? TEST_OK : TEST_NG);
     printf(" TestCommandRawParseRawReply002: reply line string: expected: %s, actual: %s\n", c[i].expL, reply->lines[0]);
+
+    printf("%s", reply->types[0] == c[i].expLT ? TEST_OK : TEST_NG);
+    printf(" TestCommandRawParseRawReply002: reply line type: expected: %s, actual: %s\n",
+        getReplyTypeCode(c[i].expLT), getReplyTypeCode(reply->types[0]));
 
     freeReply(reply);
   }
