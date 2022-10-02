3c38ef4a77dd1bd3898c2f719d6101c81c1288d8
Taishi Kasuga <supercaracal@yahoo.com>
2022-10-02 22:02:42 +0900

    fix

--- test/wait.sh
+++ test/wait.sh
@@ -10,12 +10,12 @@ for i in {0..9}; do
   cnt=0
 
   while : ; do
-    if [[ ${cnt} -gt ${MAX_ATTEMPTS} ]]; then
+    if [[ ${cnt} -ge ${MAX_ATTEMPTS} ]]; then
       echo "Max attempts exceeded: $cnt times"
       break
     fi
 
-    if docker compose exec src1 redis-cli -c get "key${i}" | grep -q 'null'; then
+    if docker compose exec src1 redis-cli -c get "key${i}" | grep -q 'nil'; then
       echo "key${i}: src: OK"
     else
       echo "key${i}: src: NG"
@@ -24,7 +24,7 @@ for i in {0..9}; do
       continue
     fi
 
-    if docker compose exec dest1 redis-cli -c get "key${i}" | grep -q 'null'; then
+    if docker compose exec dest1 redis-cli -c get "key${i}" | grep -q 'nil'; then
       echo "key${i}: dest: OK"
     else
       echo "key${i}: dest: NG"
